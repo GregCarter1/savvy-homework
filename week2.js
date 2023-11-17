66b6aca32f61328e94d75dd30112502ab7aece99
Greg Carter <greg.a.carter1@gmail.com>
2023-11-16 21:42:27 -0500

    week 2 homework submission

---
+++ week2.js
@@ -0,0 +1,19 @@
+let pizzaPlace = "Anotnio's Pizza";
+let numberOfToppings = 10;
+
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+`Hello, here at ${pizzaPlace} we have up to ${numberOfToppings}`;
+
+if (numberOfToppings < 10) {
+  console.log("Quality over quantity");
+} else {
+    console.log("A Whole lot of pizza");
+  }
+
+let x = 2;
+while (x <= 10) {
+  console.log(x);
+  x++
+}
